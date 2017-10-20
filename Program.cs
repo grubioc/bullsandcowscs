c80a9e91ae2b20500a0c2377f810b700f750e9f4
GRC <grubioc@gmail.com>
2017-10-20 15:23:46 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace BullsAndCows
+{
+    class Program
+    {
+
+        const int NUMBER_OF_GUESS = 5;
+        static void Main(string[] args)
+        {
+            
+            WelcomeMessage();
+            AskForGuess();
+            
+            Console.ReadLine();
+        }
+
+        static void WelcomeMessage(){
+            //Welcome message
+            Console.WriteLine("Welcome to the Bulls and Cows Game !");
+            Console.WriteLine("You have " + NUMBER_OF_GUESS + " attempts to guess my secret word!");
+
+        }
+
+        static string AskForGuess(){
+            //Ask for guess
+            string myGuess = "";
+            Console.WriteLine("Please guess: ");
+            myGuess = Console.ReadLine();
+            
+            //Display guess
+            Console.WriteLine("Your guess was :" + myGuess);
+            return myGuess;
+        }
+        
+    }
+}
